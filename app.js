a6d09b8e6d267496008f7f4608970454306cd042
Davi Corrêa <davi.correa.ribeiro@gmail.com>
2026-09-10 23:10:07 -0300

    Desenvolvi o formulário para o cadastro e login

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const path = require('path');
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 // dotenv require & db require
-require('dotenv').config();
-const db = require('./src/cofig/database');
+// require('dotenv').config();
+// const db = require('./src/cofig/database');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,13 +18,25 @@ app.get(('/'), (req, res) => {
     res.render('root');
 });
 
+app.post(('/'), (req, res) => {
+    const data = req.body;
 
+    if ((data.username != '') && (data.password != '')) {
+        console.log("Deu certo");
+        res.render('catalogo');
+    } else if ((data.newUser != '') && (data.newPassword != '')) {
+        console.log("Deu certo");
+        res.render('catalogo');
+    } else {
+        console.log("Deu ruim");
+    };
 
+});
 
 // The LOGIN route
 app.get(('/catalogo'), (req, res) => {
-    res.render('catalogo')
-})
+    res.render('catalogo');
+});
 
 
 
